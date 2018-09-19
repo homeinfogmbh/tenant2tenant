902e99da592d6629b9ecca25f952484e838b3bb5
Richard Neumann <r.neumann@homeinfo.de>
2018-09-19 17:46:37 +0200

    Added email to __all__.

--- tenant2tenant/__init__.py
+++ tenant2tenant/__init__.py
@@ -1,7 +1,8 @@
 """Microservice for tenant-to-tenant messages."""
 
+from tenant2tenant.email import email
 from tenant2tenant.orm import TenantMessage
 from tenant2tenant.wsgi import APPLICATION
 
 
-__all__ = ['APPLICATION', 'TenantMessage']
+__all__ = ['APPLICATION', 'email', 'TenantMessage']

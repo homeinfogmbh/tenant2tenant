4da466515a00424d0fd43ab32656ff0c7cd1f59b
Richard Neumann <r.neumann@homeinfo.de>
2018-09-20 14:08:51 +0200

    Refac.

--- tenant2tenant/email.py
+++ tenant2tenant/email.py
@@ -4,6 +4,7 @@ from emaillib import EMail, Mailer
 from functoolsplus import coerce
 
 from tenant2tenant.config import CONFIG
+from tenant2tenant.orm import NotificationEmail
 
 
 __all__ = ['email']
@@ -20,6 +21,9 @@ def emails(message):
             NotificationEmail.customer == message.customer):
         recipient = email.email
         subject = email.subject or CONFIG['email']['subject']
+        address = message.address
+        subject = subject.format(address.street, address.house_number)
+        sender = CONFIG['email']['from']
         html = message.message if email.html else None
         plain = None if email.html else message.message
         yield EMail(subject, sender, recipient, plain=plain, html=html)

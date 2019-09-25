ea67dd9aa45f539e7d2822fd3dfe5b689af956a6
Richard Neumann <r.neumann@homeinfo.de>
2019-09-25 13:05:46 +0200

    Added preview.

--- tenant2tenant/wsgi.py
+++ tenant2tenant/wsgi.py
@@ -3,9 +3,12 @@
 from flask import request
 
 from his import CUSTOMER, authenticated, authorized, Application
+from his.messages.request import INVALID_CONTENT_TYPE
 from notificationlib import get_wsgi_funcs
-from wsgilib import JSON
+from previewlib import preview, DeploymentPreviewToken
+from wsgilib import ACCEPT, JSON, XML
 
+from tenant2tenant.dom import tenant2tenant     # pylint: disable=E0401,E0611
 from tenant2tenant.messages import MESSAGE_TOGGLED
 from tenant2tenant.messages import MESSAGE_PATCHED
 from tenant2tenant.messages import MESSAGE_DELETED
@@ -112,12 +115,35 @@ def delete_message(ident):
 GET_EMAILS, SET_EMAILS = get_wsgi_funcs('tenant2tenant', NotificationEmail)
 
 
-ROUTES = (
+@preview(DeploymentPreviewToken)
+def preview_(deployment):
+    """Returns the tenant-to-tenant preview
+    messages for the requested deployment.
+    """
+
+    messages = TenantMessage.for_deployment(deployment)
+
+    if  'application/xml' in ACCEPT or '*/*' in ACCEPT:
+        xml = tenant2tenant()
+
+        for message in messages:
+            xml.message.append(message.to_dom())
+
+        return XML(xml)
+
+    if 'application/json' in ACCEPT:
+        return JSON([message.to_json() for message in messages])
+
+    return INVALID_CONTENT_TYPE
+
+
+APPLICATION.add_routes((
     ('GET', '/message', list_messages, 'list_messages'),
     ('GET', '/message/<int:ident>', get_message, 'get_message'),
     ('PUT', '/message/<int:ident>', toggle_message, 'toggle_message'),
     ('PATCH', '/message/<int:ident>', patch_message, 'patch_message'),
     ('DELETE', '/message/<int:ident>', delete_message, 'delete_message'),
     ('GET', '/email', GET_EMAILS, 'get_emails'),
-    ('POST', '/email', SET_EMAILS, 'set_emails'))
-APPLICATION.add_routes(ROUTES)
+    ('POST', '/email', SET_EMAILS, 'set_emails'),
+    ('GET', '/preview', preview_)
+))

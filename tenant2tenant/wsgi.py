d5181d98505a0bf4b7fc48bc06abb929f7080e82
Richard Neumann <r.neumann@homeinfo.de>
2019-09-25 16:59:29 +0200

    Removed unnecessary preview stuff.

--- tenant2tenant/wsgi.py
+++ tenant2tenant/wsgi.py
@@ -3,17 +3,15 @@
 from flask import request
 
 from his import CUSTOMER, authenticated, authorized, Application
-from his.messages.request import INVALID_CONTENT_TYPE
 from notificationlib import get_wsgi_funcs
-from previewlib import preview, DeploymentPreviewToken
-from wsgilib import ACCEPT, JSON, XML
+from wsgilib import JSON
 
-from tenant2tenant.dom import tenant2tenant     # pylint: disable=E0401,E0611
 from tenant2tenant.messages import MESSAGE_TOGGLED
 from tenant2tenant.messages import MESSAGE_PATCHED
 from tenant2tenant.messages import MESSAGE_DELETED
 from tenant2tenant.messages import NO_SUCH_MESSAGE
 from tenant2tenant.orm import TenantMessage, NotificationEmail
+
 
 __all__ = ['APPLICATION']
 
@@ -115,28 +113,6 @@ def delete_message(ident):
 GET_EMAILS, SET_EMAILS = get_wsgi_funcs('tenant2tenant', NotificationEmail)
 
 
-@preview(DeploymentPreviewToken)
-def deployment_preview(deployment):
-    """Returns the tenant-to-tenant preview
-    messages for the requested deployment.
-    """
-
-    messages = TenantMessage.for_deployment(deployment)
-
-    if  'application/xml' in ACCEPT or '*/*' in ACCEPT:
-        xml = tenant2tenant()
-
-        for message in messages:
-            xml.message.append(message.to_dom())
-
-        return XML(xml)
-
-    if 'application/json' in ACCEPT:
-        return JSON([message.to_json() for message in messages])
-
-    return INVALID_CONTENT_TYPE
-
-
 APPLICATION.add_routes((
     ('GET', '/message', list_messages, 'list_messages'),
     ('GET', '/message/<int:ident>', get_message, 'get_message'),
@@ -144,6 +120,5 @@ APPLICATION.add_routes((
     ('PATCH', '/message/<int:ident>', patch_message, 'patch_message'),
     ('DELETE', '/message/<int:ident>', delete_message, 'delete_message'),
     ('GET', '/email', GET_EMAILS, 'get_emails'),
-    ('POST', '/email', SET_EMAILS, 'set_emails'),
-    ('GET', '/preview/deployment', deployment_preview)
+    ('POST', '/email', SET_EMAILS, 'set_emails')
 ))

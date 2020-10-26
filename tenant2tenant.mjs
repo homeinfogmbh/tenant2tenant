471e5d5422d611e17571d920180569139f50dc16
Richard Neumann <mail@richard-neumann.de>
2020-10-26 17:32:46 +0100

    Debug.

--- tenant2tenant.mjs
+++ tenant2tenant.mjs
@@ -90,6 +90,8 @@ function delete_ (ident) {
     Lists the entries for the respective customer.
 */
 function list (entries) {
+    console.log('ENTRIES: ' + entries);
+    console.log('ENTRIES JSON: ' + JSON.stringify(entries));
 	entries.sort(function(b, a) {
 		return compareStrings(a.created, b.created);
 	});

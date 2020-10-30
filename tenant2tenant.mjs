b64647b5f6f56bc20eb55958bffe8dd7ed43e17d
Richard Neumann <mail@richard-neumann.de>
2020-10-30 18:03:55 +0100

    Added GPL header.

--- tenant2tenant.mjs
+++ tenant2tenant.mjs
@@ -1,9 +1,22 @@
 /*
-    Tenant-to-tenant java script module.
+  Tenant-to-tenant java script module.
 
-    (C) 2018-2020 HOMEINFO - Digitale Informationssysteme GmbH
+  (C) 2015-2020 HOMEINFO - Digitale Informationssysteme GmbH
 
-    Maintainer: Richard Neumann <r period neumann at homeinfo dot de>
+  This library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+  Maintainer: Richard Neumann <r dot neumann at homeinfo period de>
 */
 'use strict';
 

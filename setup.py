94c105825007a5a25d10af32e752cbc43bcec018
Richard Neumann <mail@richard-neumann.de>
2021-01-22 13:54:59 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='tenant2tenant',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'flask',
+        'functoolsplus',
+        'his',
+        'hwdb',
+        'mdb',
+        'notificationlib',
+        'peewee',
+        'peeweeplus',
+        'previewlib',
+        'wsgilib',
+    ],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='<r.neumann@homeinfo.de>',
     packages=['tenant2tenant'],
     license='GPLv3',
-    description='HIS microservice to handle tenant-to-tenant messages.')
+    description='HIS microservice to handle tenant-to-tenant messages.'
+)

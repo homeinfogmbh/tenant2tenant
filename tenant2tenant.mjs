c35b61d05764c5bb2ec583c4ac6a0ca795640d42
Richard Neumann <mail@richard-neumann.de>
2021-09-17 10:01:48 +0200

    Update copyright year

--- tenant2tenant.mjs
+++ tenant2tenant.mjs
@@ -1,7 +1,7 @@
 /*
   Tenant-to-tenant java script module.
 
-  (C) 2015-2020 HOMEINFO - Digitale Informationssysteme GmbH
+  (C) 2015-2021 HOMEINFO - Digitale Informationssysteme GmbH
 
   This library is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
